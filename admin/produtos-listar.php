8695ec53f0b3a2364e9ddadcc3cf60e1ea0a03e3
IPramanix <guilhermebarauna13@gmail.com>
2023-06-11 17:16:46 -0300

    teste

--- admin/produtos-listar.php
+++ admin/produtos-listar.php
@@ -21,7 +21,9 @@
 <html>
 <head>
   <title>Listar Produtos - Storage Mercancia</title>
-  
+
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
   <style>
     /* Estilos CSS */
@@ -138,7 +140,18 @@
             echo "<td>".$user_data['precoVenda']."</td>";
             echo "<td>".$user_data['id_fornecedor']."</td>";
             echo "<td>
-              
+              <a class='btn btn-sm' href='produtos-editar.php?id_produto=$user_data[id_produto]'>
+              <svg xmlns='http://www.w3.org/2000/svg' width='25' height='25' fill='currentColor' class='bi bi-pencil-square' viewBox='0 0 15 15'>
+              <path d='M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z'/>
+              <path fill-rule='evenodd' d='M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z'/>
+              </svg>
+              </a>
+              <a class='btn btn-sm' href='deleteprod.php?id_produto=$user_data[id_produto]'>
+              <svg xmlns='http://www.w3.org/2000/svg' width='25' height='25' fill='currentColor' class='bi bi-backspace' viewBox='0 0 15 15'>
+              <path d='M5.83 5.146a.5.5 0 0 0 0 .708L7.975 8l-2.147 2.146a.5.5 0 0 0 .707.708l2.147-2.147 2.146 2.147a.5.5 0 0 0 .707-.708L9.39 8l2.146-2.146a.5.5 0 0 0-.707-.708L8.683 7.293 6.536 5.146a.5.5 0 0 0-.707 0z'/>
+              <path d='M13.683 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-7.08a2 2 0 0 1-1.519-.698L.241 8.65a1 1 0 0 1 0-1.302L5.084 1.7A2 2 0 0 1 6.603 1h7.08zm-7.08 1a1 1 0 0 0-.76.35L1 8l4.844 5.65a1 1 0 0 0 .759.35h7.08a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1h-7.08z'/>
+              </svg>
+              </a>
             </td>";
             echo "</tr>";
           }
